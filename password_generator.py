33b899c1b4accfc535ae99ba4eae44fde403abb7
bhavanabanuri <89377079+bhavanabanuri@users.noreply.github.com>
2022-07-24 18:57:06 +0530

    Update password_generator.py
    Initial Commit

--- password_generator.py
+++ password_generator.py
@@ -1,41 +1,30 @@
-from tkinter import*
-from random import choice
 import string
+import random
 
-class App:
-    def __init__(self):
-        self.window = Tk()
-        self.window.title('password_generator')
-        self.window.iconbitmap('logo.ico')
-        self.window.iconphoto(False, PhotoImage(file='logo.png'))
-        self.window.geometry('500x255')
-        self.window.config(bg='gray')
 
-        #component creation
-        self.label()
-        self.entry()
-        self.button()
+## characters to generate password from
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
-    def label(self):
-        label_title = Label(self.window, text='Welcome to password generator', font=('Courrier', 20), bg='gray', fg='black')
-        label_title.pack()
+def generate_random_password():
+	## length of password from the user
+	length = int(input("Enter password length: "))
 
-    def entry(self):
-        self.password_entry = Entry(self.window, font=('Courrier', 25), bg='white', fg='black', width=30, relief='solid')
-        self.password_entry.pack(pady=50)
+	## shuffling the characters
+	random.shuffle(characters)
+	
+	## picking random characters from the list
+	password = []
+	for i in range(length):
+		password.append(random.choice(characters))
 
-    def button(self):
-        password_generator = Button(self.window, text="Generate_password",  font=('Courrier', 12), bg='white', fg='black', width=25, command=self.generate_password)
-        password_generator.pack()
+	## shuffling the resultant password
+	random.shuffle(password)
 
-    def generate_password(self):
-        characters = string.ascii_letters + string.punctuation + string.digits
-        password = ""
-        for x in range(28):
-            password+=choice(characters)
-        self.password_entry.delete(0, END)
-        self.password_entry.insert(0, password)
-        
-#display
-app = App()
-app.window.mainloop()
+	## converting the list to string
+	## printing the list
+	print("".join(password))
+
+
+
+## invoking the function
+generate_random_password()
